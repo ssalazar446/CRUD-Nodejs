49fdd64a7a52ce325132af918cd6d0a0b24a10c9
ssalazar446 <ssalazar446@misena.edu.co>
2023-03-07 13:35:23 -0500

    video 26:38

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const exphbs = require('express-handlebars')
+const path = require('path')
 
 //initializations
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 4000)
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: 'hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 //Middlewares
+app.use(morgan('dev'));
+
+//Global Variables
+
+//Routes
+app.use(require('./routes'))
+//Public
+
+//Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+})
