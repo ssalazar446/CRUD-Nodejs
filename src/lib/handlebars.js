562207535f683e3f182682b33afb31ced01f7562
ssalazar446 <ssalazar446@misena.edu.co>
2023-03-11 20:02:18 -0500

    section 8

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -3,7 +3,8 @@ const {format} = require('timeago.js');
 const helpers = {};
 
 helpers.timeago = (savedTimestamp) => {
-    return format(savedTimestamp);
+
+    return format(savedTimestamp, 'es_ES');
 };
 
 module.exports = helpers;
--- src/routes/paquetes.js
+++ src/routes/paquetes.js
@@ -21,5 +21,29 @@ router.get('/', async (req, res) => {
     const paquetes = await pool.query('SELECT * FROM paquetes')
     console.log(paquetes);
     res.render('paquetes/list', {paquetes})
-})
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM paquetes WHERE ID = ?', [id]);
+    res.redirect('/links')
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await pool.query('SELECT * FROM paquetes WHERE id = ?', [id]);
+    console.log(links);
+    res.render('paquetes/edit', {links: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description } = req.body; 
+    const newPack = {
+        title,
+        description,
+    };
+    await pool.query('UPDATE paquetes set ? WHERE id = ?', [newPack, id]);
+    res.redirect('/links');
+});
 module.exports = router;
